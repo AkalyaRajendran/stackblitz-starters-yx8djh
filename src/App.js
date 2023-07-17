76a1cdef09d2d282536885f8539efb7e548a54d0
AkalyaRajendran <139786884+AkalyaRajendran@users.noreply.github.com>
2023-07-17 20:31:38 +0530

    added name

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './style.css';
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
+      <h1>Hello Akalya!</h1>
       <p>Start editing to see some magic will happen :)</p>
     </div>
   );
