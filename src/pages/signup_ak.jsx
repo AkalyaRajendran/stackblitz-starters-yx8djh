cad253f0393ab59f1b685490571377853ea5591e
AkalyaRajendran <139786884+AkalyaRajendran@users.noreply.github.com>
2023-07-19 20:16:54 +0530

    1 file modified

--- src/pages/signup_ak.jsx
+++ src/pages/signup_ak.jsx
@@ -1,25 +1,32 @@
-import React from "react";
-function Signup(){
+import React from 'react';
+function Signup() {
   return (
     <div>
       <div className="signup">
-        <div className="signupform">
-            <label for=""><i className="fa-solid fa-user"></i> Username</label>
-            <input type="text" placeholder="Enter username" id="name">
+        <div className="signupForhtmlm">
+          <label Forhtml="">
+            <i className="fa-solid fa-user"></i> Username
+          </label>
+          <input type="text" placeholder="Enter username" id="name" />
         </div>
-        <div className="signupform">
-            <label for=""><i className="fa-solid fa-lock"></i> Password</label>
-            <input type="text" placeholder="Enter password">
+        <div className="signupForhtmlm">
+          <label Forhtml="">
+            <i className="fa-solid fa-lock"></i> Password
+          </label>
+          <input type="text" placeholder="Enter password" />
         </div>
-        <div className="signupform">
-            <label for=""><i className="fa-solid fa-unlock"></i> Confirm Password</label>
-            <input type="text" placeholder="Re-enter password">
+        <div className="signupForhtmlm">
+          <label Forhtml="">
+            <i className="fa-solid fa-unlock"></i> Confirm Password
+          </label>
+          <input type="text" placeholder="Re-enter password" />
         </div>
-        <div className="signupform btn">
-            <button>Signup</button>
+        <div className="signupForhtmlm btn">
+          <button>Signup</button>
         </div>
+      </div>
     </div>
-    </div>
+  );
 
-  )
+  export default Signup;
 }
